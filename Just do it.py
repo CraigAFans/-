bda0f6fc0da696966d8d7816ef969ee250041b1e
CraigAFans <51067661+CraigAFans@users.noreply.github.com>
2020-02-23 00:22:49 +0800

    Update Just do it.py

--- Just do it.py
+++ Just do it.py
@@ -7,12 +7,7 @@ url = "http://www.biquge.tv/0_1/1.html"
 r = requests.get("http://www.biquge.tv/0_1/1.html", headers=headers)
 r.encoding = r.apparent_encoding
 html = r.text
-#print(html)
-#mainContext = re.findall(r'<dt>.*?</dl>', html, re.S)
 content = re.findall(r'<div id="content">(.*?)</div>', html, re.S)
-#pattern = re.match(r'<div id="content">(.*?)</div>', html)
-#content = re.findall(pattern, html)
-#print(content)
 title = re.findall(r'<h1>(.*?)</h1>', html, re.S)
 with open('E:/E-book/literature/%s.txt' % title,'w', encoding='utf-8') as f:
     for i in content:
